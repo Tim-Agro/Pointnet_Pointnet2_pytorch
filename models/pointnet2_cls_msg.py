78c32c734dd57c85e792877a4484df288e892988
Tim <tianyi.jia@wur.nl>
2026-07-02 11:43:24 +0200

    Add weighted loss for class-balanced training

--- models/pointnet2_cls_msg.py
+++ models/pointnet2_cls_msg.py
@@ -43,9 +43,11 @@ class get_loss(nn.Module):
     def __init__(self):
         super(get_loss, self).__init__()
 
-    def forward(self, pred, target, trans_feat):
-        total_loss = F.nll_loss(pred, target)
-
+    def forward(self, pred, target, trans_feat, weight=None):
+        if weight is not None:
+            total_loss = F.nll_loss(pred, target, weight=weight)
+        else:
+            total_loss = F.nll_loss(pred, target)
         return total_loss
 
 
